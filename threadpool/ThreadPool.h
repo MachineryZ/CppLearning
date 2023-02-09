618482d5bf85601b9ac813c2bd24ce224b2b84e1
MachineryZ <zhizuozhang@gmail.com>
2023-02-09 16:03:08 +0800

    update

--- threadpool/ThreadPool.h
+++ threadpool/ThreadPool.h
@@ -27,23 +27,29 @@ private:
 };
 
 inline ThreadPool::ThreadPool(size_t threads) : stop(false) {
-    for(size_t i = 0; i < threads; ++i) workers.emplace_back([this]) {
-        for(;;) {
-            std::function<void()> task; {
-                std::unique_lock<std::mutex> lock(this->queue_mutex);
-                this->condition.wait(lock, [this]{return this->stop || !this->tasks.empty();});
-                if(this->stop && this->tasks.empty())
-                    return;
-                task = std::move(this->tasks.front());
-                this->tasks.pop();
+    for(size_t i = 0; i < threads; ++i) {
+        workers.emplace_back([this]
+            {
+                for(;;) {
+                    std::function<void()> task; 
+                    {
+                        std::unique_lock<std::mutex> lock(this->queue_mutex);
+                        this->condition.wait(lock, [this]{return this->stop || !this->tasks.empty();});
+                        if(this->stop && this->tasks.empty())
+                            return;
+                        task = std::move(this->tasks.front());
+                        this->tasks.pop();
+                    }
+                    task();
+                }
             }
-        }
-        task();
+        );
     }
 }
 
-template<class F, class...Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(args...)>::type {
+template<class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args) 
+    -> std::future<typename std::result_of<F(Args...)>::type> {
     using return_type = typename std::result_of<F(Args...)>::type;
     auto task = std::make_shared< std::packaged_task<return_type()> >(
         std::bind(std::forward<F>(f), std::forward<Args>(args)...)
@@ -54,8 +60,10 @@ auto ThreadPool::enqueue(F&& f, Args&&... args) -> std::future<typename std::res
         std::unique_lock<std::mutex> lock(queue_mutex);
         if(stop)
             throw std::runtime_error("enqueue on stopped ThreadPool");
-        tasks.emplace([task](){**task)(); });
+        tasks.emplace([task](){ (*task)(); });
     }
+    condition.notify_one();
+    return res;
 }
 
 
